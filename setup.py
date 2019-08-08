39a1ea63808754a61590a72373b04a53a2331fbb
Mikael Carlavan <contact@mika-carl.fr>
2019-08-08 15:55:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="audiofeel-audiofeel",
+    name="audiofeel",
     version="0.0.1",
     author="Simon Henrot & Mikael Carlavan",
     author_email="contact@mika-carl.fr",
